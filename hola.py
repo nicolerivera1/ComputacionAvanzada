9931bb7f76dbfe194049a79db022d7fa50f76216
nicole rivera <41299759+nicolerivera1@users.noreply.github.com>
2022-09-15 11:09:40 -0500

    options reading

---
+++ hola.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+#lista los parametros de entrada
+print(sys.argv)
---
+++ hola.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo $@  #despliega lo ingresado
+echo $0  #despliega el nombre del programa
+echo $1  #despliega la primera parte del input
+echo $2  #despliega la segunda parte del input
