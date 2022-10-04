5594e33f6882f09b19c6c46e8421044967f31cb2
nicole rivera <41299759+nicolerivera1@users.noreply.github.com>
2022-10-04 10:30:48 -0500

    U1 sets generator modified

--- hola.py
+++ hola.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-import sys
+# import sys
+import numpy as np
 
+# lista los parametros de entrada
+# print(sys.argv)
 
-#lista los parametros de entrada
-print(sys.argv)
+arr = np.array([[-8, -7, 1, 5, 9], [-9, -5, -1, 7, 8], [-9, -7, 2, 4, 10],
+                [-10, -4, -2, 7, 9], [-18, -17, 1, 14, 20]])
+
+print(arr)
+
+uniques, index_unq = np.unique(np.sort(np.abs(arr)), return_index=True, axis=0)
+print(arr[index_unq])
